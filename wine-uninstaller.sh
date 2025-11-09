e7f331966c06b4ce646aa7a765b4d866d332a8a1
Noah Domingues <noahsdom@gmail.com>
2025-11-09 12:54:12 +0000

    Update wine-uninstaller.sh

--- wine-uninstaller.sh
+++ wine-uninstaller.sh
@@ -1,8 +1,11 @@
+# Remove Wine wrapper and desktop entry
 sudo rm -f /usr/local/bin/wine-wrap.sh
 sudo rm -f /usr/share/applications/wine.desktop
 sudo update-desktop-database /usr/share/applications || true
-# restore mimeapps.list backup if present
+
+# Restore mimeapps.list backup if present
 ls -1 /etc/xdg/mimeapps.list.bak*
-# optionally remove wine packages
+
+# Optionally remove wine packages
 sudo apt remove --purge -y wine64 wine32 winetricks
 sudo apt autoremove -y
